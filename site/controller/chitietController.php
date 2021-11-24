830d8e91c52403fc91f4f82be4da692ea30f05d0
unknown <phanvan0322@gmail.com>
2021-11-24 20:56:48 +0700

    chinh sua loi

--- site/controller/chitietController.php
+++ site/controller/chitietController.php
@@ -4,20 +4,21 @@ class chitietController
 
     private $sanPhamIDModel;
 
-    function __construct(){
+    function __construct()
+    {
         $this->sanPhamIDModel = model('san_phamModel');
     }
 
     public function index()
     {
-        if(isset($_GET['id'])){
+        if (isset($_GET['id'])) {
             $id = $_GET['id'];
-            $iddm = $_GET['iddm'];
+            $iddm = $_GET['id'];
             $sanPhamID = $this->sanPhamIDModel->get_sanPham($id);
             $sanPhamLQ = $this->sanPhamIDModel->get_id_dm_by_id_sanPham($iddm);
             view('trangChinh/chitietSPView', 'site', [
-                'sanPhamID'=>$sanPhamID,
-                'sanPhamLQ'=>$sanPhamLQ
+                'sanPhamID' => $sanPhamID,
+                'sanPhamLQ' => $sanPhamLQ
             ]);
         }
     }
--- site/view/trangChinh/timkiemView.php
+++ site/view/trangChinh/timkiemView.php
@@ -1,65 +1,65 @@
 <link rel="stylesheet" href="<?= URL_PUBLIC ?>site/css/search.css">
 <link rel="stylesheet" href="<?= URL_PUBLIC ?>site/css/index.css">
-      <div class="container">
-        <div class="danhMucAll sanPham showsearch">
-          <div class="menuProduct">
-            <div class="searchDanhmuc">
-              <h4>Danh mục sản phẩm</h4>
-              <ul>
-              <?php
-                foreach ($data['danhMuc'] as $danhMuc) {
-                    echo '
+<div class="container">
+  <div class="danhMucAll sanPham showsearch">
+    <div class="menuProduct">
+      <div class="searchDanhmuc">
+        <h4>Danh mục sản phẩm</h4>
+        <ul>
+          <?php
+          foreach ($data['danhMuc'] as $danhMuc) {
+            echo '
                     <li>
-                        <a href="?c=timkiem&a=dm&id='.$danhMuc[1].'">
+                        <a href="?c=timkiem&a=dm&id=' . $danhMuc[1] . '">
                             <p>' . $danhMuc[1] . '</p>
                         </a>
                     </li>
                         ';
-                }
-                ?>
-              </ul>
-            </div>
-            <div class="searchDanhmuc">
-              <h4>Sản phẩm mua nhiều</h4>
-              <ul class="productRating">
-                <li><a href="">Sản phẩm 1</a></li>
-                <li><a href="">Sản phẩm 1</a></li>
-              </ul>
-            </div>
-            <div class="searchDanhmuc">
-              <h4>Giá</h4>
-              <ul>
-                <li><a href="">Dưới 300.000 vnđ</a></li>
-                <li><a href="">Dưới 600.000 vnđ</a></li>
-                <li><a href="">Dưới 1.000.000 vnđ</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="showProduct">
-            <h3 class="searchOutput">Kết quả tìm kiếm cho "<?php echo $data['tukhoa'] ?>"</h3>
-            <div class="container-c">
-              <!-- khung sản phẩm -->
-              <?php
-              // if(isset($data['view'])){
-              //   echo 'Không có sản phẩm cần tìm';
-              // }
-              // else{
-                foreach($data['view'] as $value){
-                  $sosoa = $value[9];
-                  $check = [
-                    0 => '',
-                    1 => '',
-                    2 => '',
-                    3 => '',
-                    4 => '',
-                ];
-                for ($i = 0; $i < $sosoa; $i++) {
-                    $check[$i] = 'checked';
-                }
-                echo '
+          }
+          ?>
+        </ul>
+      </div>
+      <div class="searchDanhmuc">
+        <h4>Sản phẩm mua nhiều</h4>
+        <ul class="productRating">
+          <li><a href="">Sản phẩm 1</a></li>
+          <li><a href="">Sản phẩm 1</a></li>
+        </ul>
+      </div>
+      <div class="searchDanhmuc">
+        <h4>Giá</h4>
+        <ul>
+          <li><a href="">Dưới 300.000 vnđ</a></li>
+          <li><a href="">Dưới 600.000 vnđ</a></li>
+          <li><a href="">Dưới 1.000.000 vnđ</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="showProduct">
+      <h3 class="searchOutput">Kết quả tìm kiếm cho "<?php echo $data['tukhoa'] ?>"</h3>
+      <div class="container-c">
+        <!-- khung sản phẩm -->
+        <?php
+        // if(isset($data['view'])){
+        //   echo 'Không có sản phẩm cần tìm';
+        // }
+        // else{
+        foreach ($data['view'] as $value) {
+          $sosoa = $value[9];
+          $check = [
+            0 => '',
+            1 => '',
+            2 => '',
+            3 => '',
+            4 => '',
+          ];
+          for ($i = 0; $i < $sosoa; $i++) {
+            $check[$i] = 'checked';
+          }
+          echo '
                 <div class="box">
-                    <a href="' . URL_WEB . '?c=chitiet&a=index&id='.$value[0].'&iddm='.$value[1].'">
-                        <img src="' . URL_PUBLIC . 'site/img/product/realme3pro.png" class="product" />
+                    <a href="' . URL_WEB . '?c=chitiet&a=index&id=' . $value[0] . '&id=' . $value[1] . '">
+                        <img src="' . URL_PUBLIC . 'site/img/' . $value[5] . '" class="product" />
                         <h3 class="name">
                             <b>' . $value[3] . '</b>
                         </h3>
@@ -75,13 +75,12 @@
                     </a>
                 </div>
                 ';
-                }
-              //}
-            ?>
-              
-              <!--  -->
-            </div>
-          </div>
-        </div>
+        }
+        //}
+        ?>
+
+        <!--  -->
       </div>
-   
+    </div>
+  </div>
+</div>
