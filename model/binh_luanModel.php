82a2c36053bff3b9683c0782ba33f60e0d2d90b4
hacklord2022 <huydnmpc01663@fpt.edu.vn>
2021-11-29 14:35:25 +0700

    cc

--- model/binh_luanModel.php
+++ model/binh_luanModel.php
@@ -3,7 +3,8 @@ class binh_luanModel extends DB
 {
     function get_binhluan_by_idSP($id)
     {
-        $sql = "SELECT * FROM binh_luan WHERE id_sanpham = '$id'";
+        $sql = "SELECT * FROM binh_luan bl INNER JOIN nguoi_dung nd ON bl.ten_dang_nhap=nd.ten_dang_nhap
+                WHERE id_sanpham = '$id'";
         return $this->getData($sql);
     }
 }
