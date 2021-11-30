49facb2c0633e5399a9764aaa3faba76d9d60677
hacklord2022 <huydnmpc01663@fpt.edu.vn>
2021-11-30 14:26:52 +0700

    themtrangiohang

---
+++ site/controller/giohangController.php
@@ -0,0 +1,15 @@
+<?php
+class giohangController
+{
+    function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+            view('trangChinh/giohangView', 'site', [
+                
+            ]); 
+        } 
+}
