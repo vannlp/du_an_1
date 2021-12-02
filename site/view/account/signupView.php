ff8f2d695faa789db14181cbe31181a5843fb2ea
unknown <phanvan0322@gmail.com>
2021-12-02 19:58:47 +0700

    update

--- site/view/account/signupView.php
+++ site/view/account/signupView.php
@@ -1,4 +1,9 @@
 <link rel="stylesheet" href="<?= URL_PUBLIC ?>site/css/user.css">
+<style>
+    label.error {
+        color: red;
+    }
+</style>
 <div class="container">
     <div class="login-wrapper signup-wrapper">
         <!-- menu -->
@@ -7,7 +12,7 @@
             <h3>Đăng nhập</h3>
 
             <div class="login">
-                <form action="?c=account&a=signup" method="post">
+                <form action="?c=account&a=signup" id="form-dk" method="post">
 
                     <div class="form-group">
                         <label for="">
@@ -20,28 +25,28 @@
                         <label for="">
                             Địa chỉ Gmail
                         </label>
-                        <input type="email" name="email" id="" required="required" class="form-controll" placeholder="Tên đăng nhập">
+                        <input type="email" name="email" id="email" required="required" class="form-controll" placeholder="Tên đăng nhập">
                     </div>
 
                     <div class="form-group">
                         <label for="">
                             Tên đăng nhập
                         </label>
-                        <input type="text" name="tenDN" id="" required="required" class="form-controll" placeholder="Tên đăng nhập">
+                        <input type="text" name="tenDN" id="ten_dang_nhap" required="required" class="form-controll" placeholder="Tên đăng nhập">
                     </div>
 
                     <div class="form-group">
                         <label for="">
                             Mật khẩu
                         </label>
-                        <input type="password" name="matkhau" id="" required="required" class="form-controll" placeholder="Tên đăng nhập">
+                        <input type="password" name="matkhau" id="mat_khau" required="required" class="form-controll mat_khau" placeholder="Tên đăng nhập">
                     </div>
 
                     <div class="form-group">
                         <label for="">
                             Nhập lại mật khẩu
                         </label>
-                        <input type="password" name="matkhau1" id="" required="required" class="form-controll" placeholder="Tên đăng nhập">
+                        <input type="password" name="matkhau1" required="required" class="form-controll mat_khau" placeholder="Tên đăng nhập">
                     </div>
                     <div class="form-group">
                         <button type="submit" name="btn-signup">Đăng Ký</button>
@@ -60,3 +65,4 @@
 
 </div>
 </div>
+<script src="<?= URL_PUBLIC ?>site/js/validate.js"></script>
