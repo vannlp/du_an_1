91db244d22cb9a49ece783e69ba684da53414e99
hacklord2022 <huydnmpc01663@fpt.edu.vn>
2021-12-08 11:50:50 +0700

    timkiem

--- model/timkiemModel.php
+++ model/timkiemModel.php
@@ -2,7 +2,7 @@
 class timkiemModel extends DB{
     function tim_kiem($timkiem){
         $sql = "SELECT * FROM `san_pham` sp JOIN `danhmuc_sp` dmsp ON sp.id_dm_sp=dmsp.Id_dm_sp
-                WHERE sp.tieu_de LIKE '%$timkiem%' OR dmsp.tieu_de LIKE '%$timkiem%'";
+                WHERE sp.tieu_de LIKE '%$timkiem%' OR dmsp.tieu_de LIKE '%$timkiem%' AND sp.trang_thai = 1";
         return $this->getData($sql);
     }
     function tim_kiem_all(){
@@ -11,12 +11,12 @@ class timkiemModel extends DB{
     }
     function tim_kiem_dm($timkiem){
         $sql = "SELECT * FROM `san_pham` sp JOIN `danhmuc_sp` dmsp ON sp.id_dm_sp=dmsp.Id_dm_sp
-                WHERE sp.tieu_de LIKE '%$timkiem%' OR dmsp.tieu_de LIKE '%$timkiem%'";
+                WHERE sp.tieu_de LIKE '%$timkiem%' OR dmsp.tieu_de LIKE '%$timkiem%' AND sp.trang_thai = 1";
         return $this->getData($sql);
     }
     function tim_kiem_shop($tukhoa){
         $sql ="SELECT * FROM `san_pham` sp JOIN `nguoi_dung` nd ON sp.ten_dang_nhap=nd.ten_dang_nhap
-               WHERE sp.ten_dang_nhap = '$tukhoa'";
+               WHERE sp.ten_dang_nhap = '$tukhoa' AND sp.trang_thai = 1";
         return $this->getData($sql);
     }
     function tim_kiem_gia($id){
