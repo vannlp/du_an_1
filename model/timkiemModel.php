77bced1283ad284c88946ae877bb3309cf033912
unknown <phanvan0322@gmail.com>
2021-12-19 13:56:41 +0700

    ok

--- model/timkiemModel.php
+++ model/timkiemModel.php
@@ -1,27 +1,32 @@
 <?php
-class timkiemModel extends DB{
-    function tim_kiem($timkiem){
+class timkiemModel extends DB
+{
+    function tim_kiem($timkiem)
+    {
+        $sql = "SELECT * FROM san_pham sp JOIN danhmuc_sp dmsp ON sp.id_dm_sp=dmsp.Id_dm_sp
+                WHERE sp.tieu_de LIKE '%$timkiem%' and sp.trang_thai = 1 OR dmsp.tieu_de LIKE '%$timkiem%' and sp.trang_thai = 1";
+        return $this->getData($sql);
+    }
+    function tim_kiem_all()
+    {
+        $sql = "SELECT * FROM san_pham where trang_thai = 1";
+        return $this->getData($sql);
+    }
+    function tim_kiem_dm($timkiem)
+    {
         $sql = "SELECT * FROM `san_pham` sp JOIN `danhmuc_sp` dmsp ON sp.id_dm_sp=dmsp.Id_dm_sp
-                WHERE sp.tieu_de LIKE '%$timkiem%' OR dmsp.tieu_de LIKE '%$timkiem%' AND sp.trang_thai = 1";
+                WHERE sp.tieu_de LIKE '%$timkiem%' AND sp.trang_thai = 1 OR dmsp.tieu_de LIKE '%$timkiem%' AND sp.trang_thai = 1";
         return $this->getData($sql);
     }
-    function tim_kiem_all(){
-        $sql = "SELECT * FROM san_pham";
-        return $this->getData($sql);
-    }
-    function tim_kiem_dm($timkiem){
-        $sql = "SELECT * FROM `san_pham` sp JOIN `danhmuc_sp` dmsp ON sp.id_dm_sp=dmsp.Id_dm_sp
-                WHERE sp.tieu_de LIKE '%$timkiem%' OR dmsp.tieu_de LIKE '%$timkiem%' AND sp.trang_thai = 1";
-        return $this->getData($sql);
-    }
-    function tim_kiem_shop($tukhoa){
-        $sql ="SELECT * FROM `san_pham` sp JOIN `nguoi_dung` nd ON sp.ten_dang_nhap=nd.ten_dang_nhap
+    function tim_kiem_shop($tukhoa)
+    {
+        $sql = "SELECT * FROM `san_pham` sp JOIN `nguoi_dung` nd ON sp.ten_dang_nhap=nd.ten_dang_nhap
                WHERE sp.ten_dang_nhap = '$tukhoa' AND sp.trang_thai = 1";
         return $this->getData($sql);
     }
-    function tim_kiem_gia($id){
-        $sql = "SELECT * FROM san_pham WHERE gia_giam <= '$id'";
+    function tim_kiem_gia($id)
+    {
+        $sql = "SELECT * FROM san_pham WHERE gia_giam <= '$id' and trang_thai = 1";
         return $this->getData($sql);
     }
 }
-?>
--- site/controller/timkiemController.php
+++ site/controller/timkiemController.php
@@ -4,72 +4,75 @@ class timkiemController
     private $timkiemModel;
     private $dmSPModel;
 
-    function __construct(){
+    function __construct()
+    {
         $this->timkiemModel = model('timkiemModel');
         $this->dmSPModel = model('dm_san_phamModel');
     }
 
-     function index(){
-        if(isset($_POST['btn-timkiem'])){
+    function index()
+    {
+        if (isset($_POST['btn-timkiem'])) {
 
-            if(isset($_POST['tim-kiem'])){
+            if (!empty($_POST['tim-kiem'])) {
                 $tukhoa = $_POST['tim-kiem'];
-                $sanPham = $this->timkiemModel->tim_kiem($tukhoa); 
-            }
-            else{
+                $sanPham = $this->timkiemModel->tim_kiem($tukhoa);
+            } else {
                 $tukhoa = "Bạn chưa tìm gì";
-            }  
-        }
-        else{
+                $sanPham = $this->timkiemModel->tim_kiem_all();
+            }
+        } else {
             $tukhoa = "Bạn chưa tìm gì";
-            $sanPham = $this->timkiemModel->tim_kiem_all(); 
+            $sanPham = $this->timkiemModel->tim_kiem_all();
         }
         $dmSP = $this->dmSPModel->get_dm_sp_all();
         //Tận cùng của nổi đau
         view('trangChinh/timkiemView', 'site', [
             'danhMuc' => $dmSP,
-            'tukhoa'=>$tukhoa,
-            'view'=>$sanPham
+            'tukhoa' => $tukhoa,
+            'view' => $sanPham
         ]);
     }
-    function dm(){
-        if(isset($_GET['id'])){
+    function dm()
+    {
+        if (isset($_GET['id'])) {
             $tukhoa = $_GET['id'];
-            $sanPham = $this->timkiemModel->tim_kiem_dm($tukhoa); 
-        }
-        else{
-            $sanPham = $this->timkiemModel->tim_kiem_all(); 
+            $sanPham = $this->timkiemModel->tim_kiem_dm($tukhoa);
+        } else {
+            $sanPham = $this->timkiemModel->tim_kiem_all();
         }
         $dmSP = $this->dmSPModel->get_dm_sp_all();
         //Tận cùng của nổi đau
         view('trangChinh/timkiemView', 'site', [
             'danhMuc' => $dmSP,
-            'tukhoa'=>$tukhoa,
-            'view'=>$sanPham
+            'tukhoa' => $tukhoa,
+            'view' => $sanPham
         ]);
     }
-    function shop(){
-        if(isset($_GET['id'])){
+    function shop()
+    {
+        if (isset($_GET['id'])) {
             $tukhoa = $_GET['id'];
             $sanPham = $this->timkiemModel->tim_kiem_shop($tukhoa);
         }
         $dmSP = $this->dmSPModel->get_dm_sp_all();
         view('trangChinh/timkiemView', 'site', [
             'danhMuc' => $dmSP,
-            'tukhoa'=>$tukhoa,
-            'view'=>$sanPham
+            'tukhoa' => $tukhoa,
+            'view' => $sanPham
         ]);
     }
-    function gia(){
-        if(isset($_GET['id'])){
+    function gia()
+    {
+        if (isset($_GET['id'])) {
             $tukhoa = $_GET['id'];
             $sanPham = $this->timkiemModel->tim_kiem_gia($tukhoa);
         }
         $dmSP = $this->dmSPModel->get_dm_sp_all();
         view('trangChinh/timkiemView', 'site', [
             'danhMuc' => $dmSP,
-            'tukhoa'=>$tukhoa,
-            'view'=>$sanPham
+            'tukhoa' => $tukhoa,
+            'view' => $sanPham
         ]);
     }
 }
